7a6783d68ee0e95afe9b9a446064c3d6241e0fa1
QamarFarooq <ch.farooq.qamar@hotmail.com>
2018-12-06 11:40:01 -0500

    Update thinning.py

--- thinning.py
+++ thinning.py
@@ -1,6 +1,45 @@
-import cv2
-import numpy as np
+from skimage.morphology import skeletonize
+from skimage import data
+import matplotlib.pyplot as plt
+from skimage.util import invert
+from skimage import data, io
 
-img = cv2.imread('j.png',0)
-kernel = np.ones((5,5),np.uint8)
-erosion = cv2.erode(img,kernel,iterations = 1)
+#----------------------------
+
+from skimage.color import rgb2gray
+from skimage import data, io
+from matplotlib import pyplot as plt
+from skimage import novice
+import numpy as np
+from skimage.io import imsave, imread
+import scipy
+import os
+
+from skimage import measure
+from skimage import filters
+
+#read image
+image_ori = io.imread('Numbers.bmp')	
+
+#convert image into greyscale
+image = invert(rgb2gray(image_ori))
+
+#perform skeletonization
+skeleton = skeletonize(image)
+
+# display results
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4),
+                         sharex=True, sharey=True)
+						 				 
+ax = axes.ravel()
+
+ax[0].imshow(image, cmap=plt.cm.gray)
+ax[0].axis('off')
+ax[0].set_title('original', fontsize=20)
+
+ax[1].imshow(skeleton, cmap=plt.cm.gray)
+ax[1].axis('off')
+ax[1].set_title('skeleton', fontsize=20)
+
+fig.tight_layout()
+plt.show()
